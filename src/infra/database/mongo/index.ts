e0126438f960b15d43526b0bfe78f1a9ab59ab63
fabiotbraga <fabiotbraga@gmail.com>
2022-08-16 23:53:43 -0300

    feat: first mongodbconnection

---
+++ src/infra/database/mongo/index.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+import 'dotenv/config';
+
+class Database {
+  constructor () {
+    this.connect();
+  }
+  connect () {
+    mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}.kdjpxpk.mongodb.net/${process.env.DB_NAME}`);
+    mongoose.connection.on('error', console.log.bind(console, 'Error connection'));
+    mongoose.connection.once('open', () => {
+      console.log('Connected database.');
+    });
+    return mongoose.connection;
+  }
+}
+
+export default new Database().connect;
