7a23cb1856320ac37b8eb140d3a9fb28c7519033
fabiotbraga <fabiotbraga@gmail.com>
2022-09-06 20:53:50 -0300

    feat: add bearer-token in routes

--- src/routes/product.router.ts
+++ src/routes/product.router.ts
@@ -5,18 +5,19 @@ import UpdateProductValidation from '../app/validations/product/UpdateProductVal
 import PatchUpdateProductValidation from '../app/validations/product/PatchUpdateProductValidation';
 import FindByIdProductValidation from '../app/validations/product/FindByIdValidation';
 import deleteProductValidation from '../app/validationS/product/DeleteProductValidation';
+import authenticate from '../app/middlewares/auth'
 import multer from 'multer';
 
 const router = Router();
 const multerConfig = multer();
 const mainRoute = '/api/v1/product';
 
-router.post(`${mainRoute}/csv`,multerConfig.single("file"), ProductController.csv);
-router.post(`${mainRoute}`, ProductValidation, ProductController.create);
-router.patch(`${mainRoute}/:id`, PatchUpdateProductValidation, ProductController.update);
-router.put(`${mainRoute}/:id`, UpdateProductValidation, ProductController.update);
-router.get(`${mainRoute}/low_stock`, ProductController.lowStock);
-router.get(`${mainRoute}/:id`, FindByIdProductValidation, ProductController.findById);
-router.get(`${mainRoute}`, ProductController.findAll);
-router.delete(`${mainRoute}/:id`, deleteProductValidation, ProductController.delete);
+router.post(`${mainRoute}/csv`, authenticate, multerConfig.single("file"), ProductController.csv);
+router.post(`${mainRoute}`, authenticate, ProductValidation, ProductController.create);
+router.patch(`${mainRoute}/:id`, authenticate, PatchUpdateProductValidation, ProductController.update);
+router.put(`${mainRoute}/:id`, authenticate, UpdateProductValidation, ProductController.update);
+router.get(`${mainRoute}/low_stock`, authenticate, ProductController.lowStock);
+router.get(`${mainRoute}/:id`, authenticate, FindByIdProductValidation, ProductController.findById);
+router.get(`${mainRoute}`, authenticate, ProductController.findAll);
+router.delete(`${mainRoute}/:id`, authenticate, deleteProductValidation, ProductController.delete);
 export default router;
